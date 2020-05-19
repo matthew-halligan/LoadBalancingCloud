861ac851b646f71800c3b436a7cc827b86665c0d
Matt Halligan <halligansventures@gmail.com>
2020-05-19 15:17:52 -0400

    Added Object Mapping PseudoCode

--- src/TicketGrantingServer.java
+++ src/TicketGrantingServer.java
@@ -1,14 +1,21 @@
 public class TicketGrantingServer {
 
     // ** CLASS PRIVATES ** //
-    //
+    // Shared key with Auth Server
 
     // ** CLASS PUBLIC METHODS ** //
-    //  Decryption Of Key*ST Message and processing
-    //
+    //  Open Socket for incoming messages
+
 
     // ** CLASS PRIVATE METHODS ** //
-    //
+    //  Master logic Method
+    //  Decryption Of Key*ST Message (Ticket Granting Ticket)
+    //  Decryption of Resource Request Message
+    //  Verification of Ticket Granting Ticket
+    //  Verification of Resource Request Message
+    //  Valid session key generation
+
+
 
 }
 
